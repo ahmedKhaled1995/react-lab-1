24b2a5cb05473b4a3bf975c6cecb775f2c4bcd22
ahmedKhaled1995 <ahmed@DESKTOP-Q38GDJO.localdomain>
2021-04-24 13:45:04 +0200

    Added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-// import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-
+      <Header title={"Blog Post"}></Header>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./header.css";
+
+const Header = (props) => {
+    return (
+        <div className="header">
+            <h2>{props.title}</h2>
+        </div>
+    )
+}
+
+export default Header
