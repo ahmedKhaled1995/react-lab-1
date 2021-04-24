ac01dfa6423025577a82b9235bedd3784bde922a
ahmedKhaled1995 <ahmed@DESKTOP-Q38GDJO.localdomain>
2021-04-24 14:45:16 +0200

    Fixed bug

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,10 @@
 const Post = (props) => {
     return (
-    
+
         <div>
-        <p>{props.post.userId}</p>
-        <p>{props.post.title}</p>
+            <p>{props.post.userId}</p>
+            <p>{props.post.title}</p>
         </div>
-        
     )
 }
 export default Post
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,12 +1,13 @@
 import Post from "./Post"
 
 const Posts = (props) => {
+    console.log(props.posts);
     return (
         <ul>
-            {props.posts.map((post)=>{
-                <li><Post post={post}/></li>
+            {props.posts.map((post) => {
+                return <li key={post.id}><Post post={post} /></li>
             })}
         </ul>
     )
 }
-export default Posts
+export default Posts;
